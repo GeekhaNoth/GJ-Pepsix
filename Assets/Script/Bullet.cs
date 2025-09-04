3d9b29579cf7daa4f7acf246c3cca7fa9aa44521
Valkora8 <matteo7882@gmail.com>
2025-09-05 01:44:09 +0200

    tirer tue les mobs

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Bullet : MonoBehaviour
 {
@@ -6,12 +7,28 @@ public class Bullet : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(Autodestruction());
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime, Space.Self);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Vérifie si l'objet avec lequel il entre en collision a un certain tag
+        if (other.gameObject.CompareTag("Mobs"))
+        {
+            other.gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Autodestruction()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
     }
 }
