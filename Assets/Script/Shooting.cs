4aac3a0c8863ffc9558cab83f9e85ac5e234cf5a
Valkora8 <matteo7882@gmail.com>
2025-09-05 01:38:46 +0200

    tirer

---
+++ Assets/Script/Shooting.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject projectile;
+    public float atkSpeed;
+    private Coroutine fireCoroutine;
+    private bool canShoot = true;
+    private bool fireCancel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canShoot && fireCoroutine == null)
+        {
+            fireCoroutine = StartCoroutine(Fire());
+        }
+        else if (Input.GetMouseButtonDown(0) && !canShoot && fireCoroutine == null)
+        {
+            fireCancel = true;
+        }
+        else if (Input.GetMouseButtonUp(0) && fireCoroutine != null)
+        {
+            StopCoroutine(fireCoroutine);
+            fireCoroutine = null;
+            StartCoroutine(FireRate());
+        }
+    }
+
+    public IEnumerator Fire()
+    {
+
+        while (true)
+        {
+            Vector2 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, angle)));
+            yield return new WaitForSeconds(atkSpeed);
+        }
+    }
+
+    public IEnumerator FireRate()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(atkSpeed);
+        canShoot = true;
+        if (fireCancel)
+        {
+            fireCancel = false;
+            fireCoroutine = StartCoroutine(Fire());
+        }
+    }
+}
